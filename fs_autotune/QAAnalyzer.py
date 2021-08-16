0a1b896a46661108d3fe3b0850525b7a31dd450e
bingze <l18081028@gs.ncku.edu.tw>
2021-08-16 15:20:11 +0800

    debug

--- fs_autotune/QAAnalyzer.py
+++ fs_autotune/QAAnalyzer.py
@@ -331,8 +331,8 @@ class QAAnalyzerBase:
         else:
             fig, ax  = plt.subplots()
             ax.scatter(cal_CD, cal_SP, s = cal_diffscale* 100, c=np.sign(cal[:,1]), cmap=cm.plasma_r, alpha=0.3)
-            ax.set_xlim(0,1000)
-            ax.set_ylim(0,1000)
+            ax.set_xlim(0,np.max(cal_CD)+100)
+            ax.set_ylim(0,np.max(cal_SP)+100)
             if select:
                 return ax, fig
         if show:
